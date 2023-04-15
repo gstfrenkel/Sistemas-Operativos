440ca159a7a0df90a55b95f861a770212d0f2b24
gstfrenkel <102612171+gstfrenkel@users.noreply.github.com>
2023-04-15 17:06:50 -0300

    Entrega final

--- Lab Fork/Prime/primes.c
+++ Lab Fork/Prime/primes.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,67 +10,79 @@ const int FORK_HIJO = 0;
 const int FIN_DE_ARCHIVO = 0;
 const int PRIMER_NUMERO = 2;
 
-void numeros_primos(int fds_original[CANALES]){
-    int primo = 0;
+void numeros_primos(int fds_original[CANALES]);
 
-    close(fds_original[1]);
+// Muestra por pantalla el primer número contenido en fds_original y se invoca
+// recursivamente con aquellos números que no sean múltiplo del número impreso.
+void
+numeros_primos(int fds_original[CANALES])
+{
+	int primo = 0;
 
-    if(read(fds_original[0], &primo, sizeof(primo)) == FIN_DE_ARCHIVO){
-        close(fds_original[0]);
-        return;
-    }
+	close(fds_original[1]);
 
-    printf("%i\n", primo);
+	if (read(fds_original[0], &primo, sizeof(primo)) == FIN_DE_ARCHIVO) {
+		close(fds_original[0]);
+		return;
+	}
 
-    int fds[CANALES];
-    int pipe_aux = pipe(fds);
+	printf("primo %i\n", primo);
 
-    if(pipe_aux == ERROR){
-        perror("Error al abrir pipe.");
-        return;
-    }
+	int fds[CANALES];
+	int pipe_aux = pipe(fds);
 
-    int fk = fork();
+	if (pipe_aux == ERROR) {
+		perror("Error al abrir pipe.\n");
+		return;
+	}
 
-    if(fk != FORK_HIJO){            
-        int recibido = 0;
+	int fk = fork();
 
-        while(read(fds_original[0], &recibido, sizeof(recibido)) != FIN_DE_ARCHIVO){
-            if(recibido % primo != 0)
-                write(fds[1], &recibido, sizeof(recibido));
-        }
+	if (fk != FORK_HIJO) {
+		int recibido = 0;
 
-        close(fds_original[0]);
-        close(fds[1]);
-        close(fds[0]);         
-    } else{
-        close(fds_original[0]);
+		while (read(fds_original[0], &recibido, sizeof(recibido)) !=
+		       FIN_DE_ARCHIVO) {
+			if (recibido % primo != 0)
+				write(fds[1], &recibido, sizeof(recibido));
+		}
 
-        numeros_primos(fds);    
-    }
+		close(fds_original[0]);
+		close(fds[1]);
+		close(fds[0]);
+
+		wait(NULL);
+	} else {
+		close(fds_original[0]);
+
+		numeros_primos(fds);
+	}
 }
 
-int main(int argc, char** argv){
-    int ingresado = atoi(argv[1]);
-    int fds[CANALES];
-    int pipe_original = pipe(fds);
+int
+main(int argc, char **argv)
+{
+	int ingresado = atoi(argv[1]);
+	int fds[CANALES];
+	int pipe_original = pipe(fds);
 
-    if(pipe_original == ERROR){
-        perror("Error al abrir pipe.");
-        return ERROR;
-    }
+	if (pipe_original == ERROR) {
+		perror("Error al abrir pipe.\n");
+		return ERROR;
+	}
 
-    int fk = fork();
+	int fk = fork();
 
-    if(fk != FORK_HIJO){
-        for(int i = PRIMER_NUMERO; i <= ingresado; i++)
-            write(fds[1], &i, sizeof(i));
+	if (fk != FORK_HIJO) {
+		for (int i = PRIMER_NUMERO; i <= ingresado; i++)
+			write(fds[1], &i, sizeof(i));
 
-        close(fds[0]);    
-        close(fds[1]);
-    } else{
-        numeros_primos(fds);
-    }
+		close(fds[0]);
+		close(fds[1]);
 
-    return EXITO;
+		wait(NULL);
+	} else
+		numeros_primos(fds);
+
+	return EXITO;
 }
